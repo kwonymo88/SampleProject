9a0f31bb6e3170418bcb8d9a585d7c3425a109a6
kwonymo88 <39462665+kwonymo88@users.noreply.github.com>
2023-12-25 23:38:23 +0900

    TSingleton 추가

---
+++ Source/TZCore/Public/Pattern/Singleton.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+
+template<typename T>
+class TSingleton : public FNoncopyable
+{
+public:
+	virtual ~TSingleton()
+	{
+		DestroyInstance();
+	}
+
+	static bool IsNullInstance() { return (nullptr == _Instance); }
+
+	static T& Get()
+	{
+		if (nullptr == _Instance)
+		{
+			_Instance = NewObject<T>();
+			if (_Instance)
+			{
+				_Instance->AddToRoot();
+			}
+		}
+		ensure(_Instance);
+
+		return *_Instance;
+	};
+
+	static void DestroyInstance()
+	{
+		if (_Instance)
+		{
+			_Instance->RemoveFromRoot();
+			_Instance = nullptr;
+		}
+	};
+private:
+	static inline T* _Instance = nullptr;
+};
